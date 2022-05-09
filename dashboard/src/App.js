bf997e69e63df005d6d4bacb9c91b8def6df9bca
infoinsec <53850514+infoinsec@users.noreply.github.com>
2022-05-08 22:28:35 -0400

    dashboard progress

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,39 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import { useInterval } from './utils.mjs'
+import { Group } from '@visx/group';
+import { Bar } from '@visx/shape';
+import { scaleLinear, scaleBand } from '@visx/scale';
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
 
-  useEffect(() => {
-    fetch("http://158.101.97.67:3000/data.json")
-    // fetch("https://api.example.com/items")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
+function getData() {
+  fetch("http://158.101.97.67:3000/data.json")
+  // fetch("https://api.example.com/items")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true)
+        setItems(JSON.parse(result))
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setIsLoaded(true)
+        setError(error)
+      }
+    )
+}
+
+// getData()
+// useInterval(() => {
+//   getData()
+// }, 1000)
+
+useEffect(() => {
+    getData()
+    const interval = setInterval(() => {
+      console.log('This will run every second!')
+      getData() 
+    }, 1000)
+    return () => clearInterval(interval)
   }, [])
-
+  
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   } else {
     return (
       <div className="App">
-        ${items}
+        <Chart data="myData"/>
+        <br />
+        {JSON.stringify(Object.keys(items))}
 
       </div>
       // <ul>
@@ -43,8 +63,12 @@ function App() {
       //     </li>
       //   ))}
       // </ul>
-    );
+    )
   }
 }
 
-export default App;
+const Chart = props => {
+  return props.data
+}
+
+export default App
