db96f14238a3f61fc0af63f69d598633b6d7d81d
infoinsec <53850514+infoinsec@users.noreply.github.com>
2022-05-14 15:48:32 -0400

    Update index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin: *')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     // Request headers you wish to allow
